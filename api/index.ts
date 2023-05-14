514100454ab6cf4d42a0aaa5060890b9c6457ca2
Alexander Zibert <alexander.zibert@googlemail.com>
2023-05-14 21:29:49 +0200

    add api

--- api/index.ts
+++ api/index.ts
@@ -9,25 +9,147 @@
  */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  // MY_KV_NAMESPACE: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+  //
+  // Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
+  // MY_SERVICE: Fetcher;
 }
 
+const hourToMs = (s: string) => {
+  const [hour, minute] = s.split(":");
+  return (Number(hour) * 3600 + Number(minute) * 60) * 1000;
+};
+
+const parseData = (txt: string) => {
+  return txt
+    .split("\n")
+    .slice(1)
+    .map((line) => {
+      return line.trim().split(";");
+    })
+    .filter((parts) => parts.length === 4)
+    .map(([date, start, end, value]) => {
+      return {
+        date,
+        start: hourToMs(start),
+        end: hourToMs(end),
+        value: Number(value.replace(",", ".")),
+      };
+    });
+};
+
+const getToday = (date: Date) => {
+  return new Intl.DateTimeFormat("de-DE", {
+    timeZone: "Europe/Berlin",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(date);
+};
+
+const beginingOfDay = (date: Date, timeZone: string) => {
+  const parts = Intl.DateTimeFormat("en-US", {
+    timeZone,
+    hourCycle: "h23",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  }).formatToParts(date);
+  if (!parts) {
+    return new Date();
+  }
+  const hour = Number(parts.find((i) => i.type === "hour")?.value);
+  const minute = Number(parts.find((i) => i.type === "minute")?.value);
+  const second = Number(parts.find((i) => i.type === "second")?.value);
+  return new Date(
+    1000 *
+      Math.floor(
+        (date.valueOf() - hour * 3600000 - minute * 60000 - second * 1000) /
+          1000
+      )
+  );
+};
+
+const getData = async function (date: Date) {
+  const start = date.valueOf() - 1 * 3600 * 1000;
+  const end = date.valueOf() + 25 * 3600 * 1000;
+
+  return fetch(
+    "https://www.smard.de/nip-download-manager/nip/download/market-data",
+    {
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({
+        request_form: [
+          {
+            format: "CSV",
+            moduleIds: [8004169],
+            region: "DE-LU",
+            timestamp_from: start,
+            timestamp_to: end,
+            type: "discrete",
+            language: "de",
+            resolution: "quarterhour",
+          },
+        ],
+      }),
+      method: "POST",
+    }
+  );
+};
+
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const { searchParams } = new URL(request.url);
+    const date = new Date(Number(searchParams.get("date")));
+    const startOfDate = beginingOfDay(date, "Europe/Berlin");
+    const today = getToday(date);
+
+    const cacheKey = new Request(
+      `https://test.com/?date=${startOfDate.toISOString()}`,
+      request
+    );
+    console.log(cacheKey);
+    const cache = caches.default;
+    let response = await cache.match(cacheKey);
+    if (response) {
+      console.log("cache hit for", cacheKey.url);
+      return response;
+    }
+
+    const fetchedResponse = await getData(startOfDate);
+
+    const data = await fetchedResponse.text();
+
+    const json = JSON.stringify({
+      date: startOfDate.valueOf(),
+      data: parseData(data)
+        .filter(({ date }) => date === today)
+        .map(({ start, end, value }) => ({ start, end, value })),
+    });
+
+    const resp = new Response(json, {
+      headers: {
+        "content-type": "application/json;charset=UTF-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+        "Access-Control-Max-Age": "86400",
+        "Cache-Control": "s-maxage=2592000",
+      },
+    });
+    ctx.waitUntil(cache.put(cacheKey, resp.clone()));
+    return resp;
+  },
 };
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,143 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h2>Datum</h2>
+  {{ formatDate(date) }}
+  <h2>Volatilität</h2>
+  <div>
+    q80 / q20:
+    {{ Math.round((quantile(0.8, data) / quantile(0.2, data)) * 100) / 100 }}
+  </div>
+  <div>
+    q90 / q10:
+    {{ Math.round((quantile(0.9, data) / quantile(0.1, data)) * 100) / 100 }}
+  </div>
 
+  <h2>Beste Zeiträume Strom zu verbrauchen</h2>
+  <div>10%: {{ formatMoney(quantile(0.1, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in lowEntries(0.1, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+  <div>20%: {{ formatMoney(quantile(0.2, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in lowEntries(0.2, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+  <div>30%: {{ formatMoney(quantile(0.3, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in lowEntries(0.3, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+
+  <h2>Schlechteste Zeiträume Strom zu verbrauchen</h2>
+  <div>90%: {{ formatMoney(quantile(0.9, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in highEntries(0.9, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+  <div>80%: {{ formatMoney(quantile(0.8, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in highEntries(0.8, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+  <div>70%: {{ formatMoney(quantile(0.7, data) / 1000) }} / kWh</div>
+  <ul>
+    <li v-for="range in highEntries(0.7, data)" :key="range.start">
+      {{ formatHour(date + range.start) }} {{ formatHour(date + range.end) }}
+    </li>
+  </ul>
+</template>
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const quantile = (q, arr) => {
+  const copy = [...arr];
+  copy.sort((a, b) => a - b);
+  const length = Math.ceil(arr.length * q);
+  return copy[length];
+};
+
+const hourFormatter = new Intl.DateTimeFormat("de-DE", {
+  timeZone: "Europe/Berlin",
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+const moneyFormatter = new Intl.NumberFormat("de-DE", {
+  style: "currency",
+  currency: "EUR",
+});
+
+const dateFormatter = new Intl.DateTimeFormat("de-DE", {
+  timeZone: "Europe/Berlin",
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
+
+const combineRanges = (data) => {
+  if (data.length === 0) {
+    return [];
+  }
+  const ranges = [{ ...data[0] }];
+  for (const entry of data.slice(1)) {
+    const lastRange = ranges[ranges.length - 1];
+    if (entry.start === lastRange.end) {
+      lastRange.end = entry.end;
+    } else {
+      ranges.push({ ...entry });
+    }
+  }
+  return ranges;
+};
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return { data: [], date: null };
+  },
+  async mounted() {
+    const data = await fetch(
+      `https://day-ahead-meter.alexander-zibert.workers.dev/?date=${new Date().valueOf()}`
+    ).then((resp) => resp.json());
+    console.log(data);
+    this.date = data.date;
+    this.data = data.data;
+  },
+  methods: {
+    formatHour(ts) {
+      return hourFormatter.format(new Date(ts));
+    },
+    formatDate(ts) {
+      return dateFormatter.format(new Date(ts));
+    },
+    formatMoney(n) {
+      return moneyFormatter.format(n);
+    },
+    quantile(q, data) {
+      return quantile(
+        q,
+        data.map(({ value }) => value)
+      );
+    },
+    lowEntries(q, data) {
+      const qs = quantile(
+        q,
+        data.map(({ value }) => value)
+      );
+      const filteredData = data.filter(({ value }) => value < qs);
+      return combineRanges(filteredData);
+    },
+    highEntries(q, data) {
+      const qs = quantile(
+        q,
+        data.map(({ value }) => value)
+      );
+      const filteredData = data.filter(({ value }) => value >= qs);
+      return combineRanges(filteredData);
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
